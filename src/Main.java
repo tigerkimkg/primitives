b86090eb2211f5b4cec6105babd90576fdd4247e
Konstantin Kim <tigerkimkg@gmail.com>
2025-07-29 11:21:08 +0300

    add main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 13676;
+        int mile = 20;
+
+        int amount = price / mile;
+
+        System.out.println("Количество бонусных миль: " + amount);
+    }
+}
